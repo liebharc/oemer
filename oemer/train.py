2deda242949adf2d400d2a100963ff7311687ef0
Christian Liebhardt <christian.liebhardt@arcor.de>
2023-11-30 17:07:55 +0100

    Added type annotations

--- oemer/train.py
+++ oemer/train.py
@@ -131,8 +131,8 @@ def batch_transform(img, trans_func):
     
 class MultiprocessingDataLoader:
     def __init__(self, num_worker: int):
-        self._queue = Queue(maxsize=20)
-        self._dist_queue = Queue(maxsize=30)
+        self._queue: Queue = Queue(maxsize=20)
+        self._dist_queue: Queue = Queue(maxsize=30)
         self._process_pool = []
         for _ in range(num_worker):
             processor = Process(target=self._preprocess_image)
